b2019ceeacf7b79ef0dc816093d2a04e4688c7ff
fanzh <fanz23@chinaunicom.cn>
2021-08-14 09:57:14 +0800

    [function] 运行时异常，非运行时异常，错误 测试

--- src/main/java/com/bruce/security/controller/CustomErrorController.java
+++ src/main/java/com/bruce/security/controller/CustomErrorController.java
@@ -1,15 +1,21 @@
 package com.bruce.security.controller;
 
+import com.bruce.security.exceptions.CustomException;
 import org.springframework.boot.autoconfigure.web.ErrorProperties;
 import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
@@ -40,5 +46,39 @@ public class CustomErrorController extends BasicErrorController {
         return new ResponseEntity<>(errorAttributes, status);
     }
 
+    @Override
+    @RequestMapping(produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView errorHtml(HttpServletRequest request, HttpServletResponse response) {
+        HttpStatus status = getStatus(request);
+        ErrorAttributeOptions options = ErrorAttributeOptions.of(ErrorAttributeOptions.Include.MESSAGE);
+        options = options.including(ErrorAttributeOptions.Include.EXCEPTION);
+        Map<String, Object> model = Collections
+                .unmodifiableMap(getErrorAttributes(request, options));
+        response.setStatus(status.value());
+        ModelAndView modelAndView = resolveErrorView(request, response, status, model);
+        return (modelAndView != null) ? modelAndView : new ModelAndView("error", model);
+    }
+
+    public static void runtimeExTest() throws CustomException {
+        throw new CustomException("test");
+    }
+
+    public static void checkExTest() throws IOException {
+        throw new IOException("test");
+    }
+
+    public static void errorTest() throws Error {
+        throw new Error("test");
+    }
+
+    public static void main(String[] args) {
+        runtimeExTest();
+        try {
+            checkExTest();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        errorTest();
+    }
 
 }
